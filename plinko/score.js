f8a21a6986696cc4ac278cae932d093054765704
Aaron <aaron@Kristins-MacBook-Air.local>
2021-02-21 20:40:07 -0700

    Implented K nearest neighbors

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,25 @@
+//store Each of the different observations that we make inside of our application
+// Implement K nearest Neighbor
+const outputs = []
+const predictionValue = 300
+const k = 3
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
-
 function runAnalysis() {
-  // Write code here to analyze stuff
+  const bucket = _.chain(outputs)
+    .map(row => [distance(row[0]), row[3]])
+    .sortBy(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value();
+    console.log("I predict that this will probably fall into bucket", bucket)
 }
-
+function distance (point) {
+  return Math.abs(point - predictionValue)
+}
